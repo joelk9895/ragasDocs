6dabd66adf55435000fd7edc946a78e28e1028fb
jjmachan <jamesjithin97@gmail.com>
2024-09-04 13:12:49 +0530

    added public as output directory

--- build_docs.sh
+++ build_docs.sh
@@ -6,4 +6,4 @@ set -x
 python3 -V
 python3 -m pip install --user poetry
 python3 -m poetry install --with docs
-python3 -m poetry run mkdocs build
+python3 -m poetry run mkdocs build --site-dir public
