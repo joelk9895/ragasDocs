abfa4b1ef5f31bafac8ac4c821aa5be676b773c7
jjmachan <jamesjithin97@gmail.com>
2024-09-04 13:10:16 +0530

    update poetry.lock

--- build_docs.sh
+++ build_docs.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
 
-# This script is used to build the documentation on CloudFlare Pages, this is just used for build previews
-# A different script with the same name exists on the `docs-site` branch (where pre-built docs live).
-
 set -e
 set -x
 
 python3 -V
-
 python3 -m pip install --user poetry
-
-python3 -m poetry install --only docs
-
-python3 -m poetry shell
-
-python3 -m mkdocs build
+python3 -m poetry install --with docs
+python3 -m poetry run mkdocs build
